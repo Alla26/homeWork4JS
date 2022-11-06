1481b46460abbc78db65ed2ce18966ba495d9d06
unknown <girlalla@yandex.ru>
2022-11-06 21:15:25 +0300

    Initial commit

---
+++ 4.objects/task.js
@@ -0,0 +1,37 @@
+function Student(name, gender, age) {
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+}
+
+Student.prototype.setSubject = function (subjectName) {
+  this.subject = subjectName;
+};
+
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) {
+    this.marks = [mark];
+  } else {
+    this.marks.push(mark);
+  }
+
+  Student.prototype.addMarks = function (...marks) {
+    if (this.marks === undefined) {
+      this.marks = [...marks];
+    } else {
+      this.marks.push(...marks);
+    }
+  };
+};
+
+Student.prototype.getAverage = function () {
+  let sum = 0;
+  this.marks.forEach((mark) => (sum += parseInt(mark)));
+  return sum / this.marks.length;
+};
+
+Student.prototype.exclude = function (reason) {
+  delete Student.subject; // либо delete this.subject;
+  delete Student.marks; // либо delete this.marks;
+  this.excluded = reason;
+};
